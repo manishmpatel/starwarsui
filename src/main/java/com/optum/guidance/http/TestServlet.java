b109ac374094853c052c2fdfddc7bae473d1c196
Patel <mpatel54@ms.ds.uhc.com>
2016-11-11 16:13:20 -0500

    initial commit

---
+++ src/main/java/com/optum/guidance/http/TestServlet.java
@@ -0,0 +1,51 @@
+package com.optum.guidance.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.swapi.models.People;
+import com.swapi.sw.StarWarsApi;
+
+
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		pw.append("Served at: ").append(request.getContextPath()).append("\n");
+		StarWarsApi api = new StarWarsApi();
+		People p = api.getPeople();
+		pw.append("Eye Color:  ").append(p.eyeColor);
+		
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
